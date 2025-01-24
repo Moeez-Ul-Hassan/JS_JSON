852a4f268a3712ace029240a726974286d074386
Moeez Ul Hassan <moeezjutt11@gmail.com>
2025-01-25 00:28:14 +0500

    Check JS added

---
+++ check.js
@@ -0,0 +1,8 @@
+fetch('check.json')
+  .then(response => response.json())
+  .then(data => {
+    console.log('Fetched JSON:', data);
+    const jsonString = JSON.stringify(data);
+    console.log('Converted JSON string:', jsonString);
+  })
+  .catch(error => console.error('Fetch error:', error));
